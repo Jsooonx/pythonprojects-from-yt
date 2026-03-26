d7490b4b2d5f40bffc73fbce91e125c59231f10d
Jsooonx <jsooonx@gmail.com>
2026-03-26 18:33:36 +0700

    Optimizing some lines in rock paper scissor

--- rock-paper-scissor.py
+++ rock-paper-scissor.py
@@ -1,7 +1,10 @@
 import random
 
-emojis = { 'r': '🪨', 's': '✂️', 'p': '📃'}
-choices =('r', 'p', 's')
+ROCK = 'r'
+SCISSORS = 's'
+PAPER = 'p'
+emojis = { ROCK: '🪨', SCISSORS: '✂️', PAPER: '📃'}
+choices = (tuple(emojis.keys()))
 
 def get_user_choice():
     while True:
@@ -19,9 +22,9 @@ def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
         print("Tie")
     elif (
-        (user_choice == 'r' and computer_choice == 's') or
-        (user_choice == 'p' and computer_choice == 'r') or
-        (user_choice == 's' and computer_choice == 'p')):
+        (user_choice == ROCK and computer_choice == SCISSORS) or
+        (user_choice == PAPER and computer_choice == ROCK) or
+        (user_choice == SCISSORS and computer_choice == PAPER)):
             print("You win!")
     else:
         print("You lose!")
