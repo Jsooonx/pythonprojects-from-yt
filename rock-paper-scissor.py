72fbe216efd21fde48ba0814a7638518f1f7fed2
Jsooonx <jsooonx@gmail.com>
2026-03-26 17:10:17 +0700

    Adding rock paper scissor

---
+++ rock-paper-scissor.py
@@ -0,0 +1,37 @@
+import random
+
+emojis = { 'r': '🪨', 's': '✂️', 'p': '📃'}
+choices =('r', 'p', 's')
+
+while True:
+    user_choice = input("Rock, Paper, or Scissors? (r/p/s): ").lower()
+
+    if user_choice not in choices:
+        print("Invalid choice!")
+        continue
+
+    computer_choice = random.choice(choices)
+
+    print(f"You chose {emojis[user_choice]}")
+    print(f"Computer chose {emojis[computer_choice]}")
+
+    if user_choice == computer_choice:
+        print("Tie")
+    elif (
+        (user_choice == 'r' and computer_choice == 's') or
+        (user_choice == 'p' and computer_choice == 'r') or
+        (user_choice == 's' and computer_choice == 'p')):
+            print("You win!")
+    else:
+        print("You lose!")
+    
+    while True:   
+        should_continue = input("Continue? (y/n): ").lower()
+        if should_continue == 'y':
+            break
+        elif should_continue == 'n':
+            print("Thanks for playing!")
+            exit()
+        else:
+            print("Invalid choice! Choose 'y' or 'n'.")
+        
